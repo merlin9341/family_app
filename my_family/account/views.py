c2c38805c1c60ea6fce1bc649046d287d46942f5
merlin9341 <felixmlroughton@gmail.com>
2024-04-24 12:09:34 +0000

    Added templates directory in project directory with base.html and test document

--- my_family/account/views.py
+++ my_family/account/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def test(request):
-    return HttpResponse("Test")
+    return render(request, "account/test.html", {})
