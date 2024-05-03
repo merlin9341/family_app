ddf7c51e312a659dd89a1404edaa45b678e3dac4
merlin9341 <felixmlroughton@gmail.com>
2024-05-03 12:57:39 +0000

    Added a basic home page view and template

--- my_family/account/urls.py
+++ my_family/account/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("test", views.test, name="test"),
+    path("", views.index, name="index"),
 ]
--- my_family/account/views.py
+++ my_family/account/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def test(request):
     return render(request, "account/test.html", {})
+
+
+def index(request):
+    """
+    A basic home page for an account
+    """
+    return render(request, "account/index.html", {})
