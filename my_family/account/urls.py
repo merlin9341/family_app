d7f4621510e4fb2f89e738b9753d89811f156553
merlin9341 <felixmlroughton@gmail.com>
2024-05-09 13:11:53 +0000

    Added auth url views and registration templates directory with test login template

--- my_family/account/urls.py
+++ my_family/account/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("test", views.test, name="test"),
     path("", views.index, name="index"),
+    path("", include("django.contrib.auth.urls"))
 ]
